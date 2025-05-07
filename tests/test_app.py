650a2e1b2c8351fe5b87c0125e463e2d8b5b81bd
frog016 <mixalyov.nikita@mail.ru>
2025-05-07 18:52:54 +0500

    Удалил лишние тесты

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,17 +13,3 @@ def test_index_route(client):
     """Test that index route returns 200"""
     response = client.get('/')
     assert response.status_code == 200
-
-def test_page_content(client):
-    """Test that page contains greeting text"""
-    response = client.get('/')
-    assert 'Привет'.encode('utf-8') in response.data
-
-def test_date_format(client):
-    """Test that date is in correct format"""
-    response = client.get('/')
-    data = response.data.decode('utf-8')
-    date_match = re.search(r'\d{2}\.\d{2}\.\d{4}', data)
-    assert date_match is not None
-    current_date = datetime.now().strftime('%d.%m.%Y')
-    assert current_date in data
