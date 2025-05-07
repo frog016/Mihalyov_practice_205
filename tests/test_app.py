6c70315767a1905b46edb601549fd8ce604d0c53
frog016 <mixalyov.nikita@mail.ru>
2025-05-07 18:58:13 +0500

    Исправил ошибку с зависимость flask

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,11 @@
-import re
-from datetime import datetime
-from app import app
 import pytest
+from app import app
 
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
 def test_index_route(client):
-    """Test that index route returns 200"""
     response = client.get('/')
     assert response.status_code == 200
